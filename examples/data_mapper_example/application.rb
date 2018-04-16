8eb1da8d0f4335fb1d439644a9a10fe19145afa5
Tiffany Ng <32461159+KimTaeyeonNg@users.noreply.github.com>
2018-04-16 14:13:23 +0800

    r

--- examples/data_mapper_example/application.rb
+++
@@ -1,5 +0,0 @@
-class DataMapperExample < Merb::Controller
-  def index
-    _('Hi! Hello world!')
-  end
-end
--- lib/merb_global.rb
+++
@@ -1,8 +0,0 @@
-require 'pathname'
-
-if defined? Merb::Plugins
-  require Pathname(__FILE__).dirname.expand_path + 'merb_global/base'
-  require Pathname(__FILE__).dirname.expand_path + 'merb_global/controller'
-
-  Merb::Plugins.add_rakefiles(Pathname(__FILE__).dirname.expand_path + 'merb_global/merbrake')
-end
